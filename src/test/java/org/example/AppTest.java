3180e98636e77e09d7da057e535759b19c61db95
Rodion Baronov <rodikno@gmail.com>
2025-09-26 23:52:15 +0200

    Add Selenium-based test and update dependencies
    Replaced legacy test framework with Selenium and TestNG for UI testing. Updated `pom.xml` to include necessary dependencies and plugins for Selenium, WebDriverManager, and TestNG. Refactored test code to include a simple input field validation.

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,38 +1,50 @@
 package org.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+import java.time.Duration;
+
+public class AppTest {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void testSimpleInput() {
+        driver.get("https://www.qa-practice.com/elements/input/simple");
+
+        WebElement input = driver.findElement(By.id("id_text_string")); // реальний id поля на цій сторінці
+        input.sendKeys("Testtest");
+
+        input.sendKeys(Keys.ENTER);
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement result = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("result-text"))); // цей id є на сторінці після сабміту
+
+        Assert.assertEquals(result.getText(), "Testtest");
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @AfterMethod
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
